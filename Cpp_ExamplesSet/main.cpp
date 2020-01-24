cbcdd1f52725c91d0f8914966067cf279865082f
chandu <kurapatichandu535@gmail.com>
2020-01-24 08:47:10 +0100

    programs are added

--- Cpp_ExamplesSet/main.cpp
+++ Cpp_ExamplesSet/main.cpp
@@ -7,6 +7,14 @@ int main()
 	//staticmemberclassexample();
 	//Inheritencesimpleexample();
 	//ClassExample1();
+	//virtualExample1();
+	//InheritenceEx1();
+	//PolymorphismEx1();
+	//ClassExample2();
+	//constructorex1();
+	//constructorex2();
+
+	referenceEx();
 
 	return 0;
 }
--- Cpp_ExamplesSet/main.h
+++ Cpp_ExamplesSet/main.h
@@ -4,3 +4,10 @@
 void staticmemberclassexample(void);
 void Inheritencesimpleexample(void);
 int ClassExample1(void);
+int virtualExample1(void);
+int InheritenceEx1(void);
+void PolymorphismEx1(void);
+void ClassExample2(void);
+void constructorex1(void);
+int constructorex2(void);
+int referenceEx(void);
